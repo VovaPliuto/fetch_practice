4c03f5b1f17fb53c97dd993371b9f3b8a0cc5c55
Seven <graf.lancelot7@gmail.com>
2023-05-13 21:35:10 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles/normalize.css";
 import "./styles/index.css";
-import { getProducts, getProductsById } from "./requests/products";
+import { getProducts, getProductsById, addProduct } from "./requests/products";
 import {
   createProductsMarkup,
   createProductMarkup,
@@ -11,9 +11,24 @@ import { renderProductsList, renderProduct } from "./services/renderMarkup";
 // getProducts().then(createProductsMarkup).then(renderProductsList);
 
 refs.getProductForm.addEventListener("submit", onGetProductSubmit); 
+refs.formThird.addEventListener("submit", onFormThirdSubmit); 
 
 function onGetProductSubmit(event) {
     event.preventDefault();
     const id = event.currentTarget.elements.id.value.trim();
     getProductsById(id).then(createProductMarkup).then(renderProduct);
 }; 
+
+function onFormThirdSubmit(event){
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const data = {};
+
+  formData.forEach((value, name)=>{
+    data[name] = value;
+  })
+
+  addProduct(data).then((response) => createProductMarkup(response.data)).then(renderProduct)
+}
+
--- src/requests/products.js
+++ src/requests/products.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const BASE_URL = "https://dummyjson.com";
 
 export function getProducts() {
@@ -5,7 +7,17 @@ export function getProducts() {
 }
 
 export function getProductsById(id) {
-  return fetch(`${BASE_URL}/products/${id}`).then((res) =>
-    res.json()
-  );
+  return fetch(`${BASE_URL}/products/${id}`).then((res) => res.json());
 }
+
+export function addProduct(data) {
+  return axios.post(`${BASE_URL}/products/add`, data);
+}
+
+// fetch('https://dummyjson.com/products/add', {
+//   method: 'POST',
+//   headers: { 'Content-Type': 'application/json' },
+//   body: JSON.stringify(data)
+// })
+// .then(res => res.json())
+// .then(console.log);
